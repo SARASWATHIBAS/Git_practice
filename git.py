7f3bd8d392990b75d2311ecffba7e86991e50f1e
SARASWATHIBAS <saraswathibas@gmail.com>
2025-06-07 17:43:55 -0400

    Resolved merge conflict

--- git.py
+++ git.py
@@ -1,7 +1,5 @@
   
 def main():
     """Checks if there's enough free memory in the computer. If so print all good"""
-    print("Hello All")
-    print("Trying to create a new conflict")
     
 main()

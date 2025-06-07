09a6b7ea7e57489599231cb2566f78923d1827da
SARASWATHIBAS <saraswathibas@gmail.com>
2025-06-07 16:12:04 -0400

    Git config added

--- git.py
+++ git.py
@@ -1,9 +1,5 @@
   
 def main():
-<<<<<<< HEAD
     """Checks if there's enough free memory in the computer. If so print all good"""
-=======
-    print("Creating a conflict")
->>>>>>> new_feature
 
 main()
